466eb0b6137f4869ea99871e21d039f8901f8bc7
Repiz <93680058+Repiz@users.noreply.github.com>
2021-11-03 21:42:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const client = new Discord.Client(
     {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "DIRECT_MESSAGE_TYPING"]}
-)
+);
 
 
 bot.login(process.env.token);
@@ -9,17 +9,17 @@ bot.login(process.env.token);
 client.on("messageCreate", (message) => {
     if (message.content == "https://tenor.com/view/sussy-baka-sus-dog-yo-momma-gif-21425378")
         message.delete();
-})
+});
 
 client.on("guildMemberAdd", member =>{
     var canale = client.channels.cache.get("905510008010178590")
-    canale.setName("👨‍🦽┊Membri: " + member.guild.memberCount)
-})
+    canale.setName("Membri: " + member.guild.memberCount)
+});
 
 client.on("guildMemberRemove", member =>{
     var canale = client.channels.cache.get("905510008010178590")
-    canale.setName("👨‍🦽┊Membri: " + member.guild.memberCount)
-})
+    canale.setName("Membri: " + member.guild.memberCount)
+});
 
 
 
