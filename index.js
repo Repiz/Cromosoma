a2a9583846d158b3b966eb09df7b74072bf1421e
Repiz <repettoalessio3@gmail.com>
2021-11-03 21:03:12 +0100

    Cromosoma

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js")
+const client = new Discord.Client(
+    {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "DIRECT_MESSAGE_TYPING"]}
+)
+
+
+client.login(process.env.token)
+
+client.on("messageCreate", (message) => {
+    if (message.content == "https://tenor.com/view/sussy-baka-sus-dog-yo-momma-gif-21425378")
+        message.delete();
+})
+
+client.on("guildMemberAdd", member =>{
+    var canale = client.channels.cache.get("905510008010178590")
+    canale.setName("👨‍🦽┊Membri: " + member.guild.memberCount)
+})
+
+client.on("guildMemberRemove", member =>{
+    var canale = client.channels.cache.get("905510008010178590")
+    canale.setName("👨‍🦽┊Membri: " + member.guild.memberCount)
+})
+
+
+
