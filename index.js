ab93e1ce4860f70aaefeb5cc537c398486887b99
Repiz <93680058+Repiz@users.noreply.github.com>
2021-11-04 15:07:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client(
 );
 
 
-client.login("OTA0Nzc0NzIxODU2NDc5MzAz.YYAbHw.-tA2bWNeFz9cTiHpPDY7XIluRn4");
+client.login(process.env.token);
 
 client.on("messageCreate", (message) => {
     if (message.content == "https://tenor.com/view/sussy-baka-sus-dog-yo-momma-gif-21425378")
