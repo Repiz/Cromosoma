6b1bccc816bd5c9fd5cd52ed33427b28ede0a05b
Repiz <93680058+Repiz@users.noreply.github.com>
2021-11-05 21:08:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const client = new Discord.Client(
     {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "DIRECT_MESSAGE_TYPING"]}
-)
+);
 
 
-client.login(process.env.token)
+client.login(process.env.token);
 
 client.on("messageCreate", (message) => {
-    if (message.content == "https://tenor.com/view/sussy-baka-sus-dog-yo-momma-gif-21425378")
+    if (message.content == "https://tenor.com/view/sussy-baka-sus-dog-yo-momma-gif-21425378"){
         message.delete();
-})
+    }
+});
 
 client.on("guildMemberAdd", member =>{
     var canale = client.channels.cache.get("905510008010178590")
-    canale.setName("👨‍🦽┊Membri: " + member.guild.memberCount)
-})
+    canale.setName("🦽┊Membri: " + member.guild.memberCount)
+});
 
 client.on("guildMemberRemove", member =>{
     var canale = client.channels.cache.get("905510008010178590")
-    canale.setName("👨‍🦽┊Membri: " + member.guild.memberCount)
-})
+    canale.setName("🦽┊Membri: " + member.guild.memberCount)
+});
+
+
+var messaggitoc = ["Testa", "Croce"];
+
+client.on ("message", (message) => {
+    if (message.content == ".teocr") {
+        var random = Math.floor(Math.random() * messaggitoc.lenght)
+        message.channel.send(messaggitoc[random] + " " + message.author.toString());
+    }
+});
+
 
 
 
