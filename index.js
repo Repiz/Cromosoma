f4eccfda4f39b863fb492ec267facdd99886e3e0
Repiz <93680058+Repiz@users.noreply.github.com>
2021-11-06 14:32:53 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const client = new Discord.Client(
     {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "DIRECT_MESSAGE_TYPING"]}
 );
 
+
 client.login(process.env.token);
 
 client.on("messageCreate", (message) => {
@@ -11,32 +12,34 @@ client.on("messageCreate", (message) => {
     }
 });
 
-var server = message.member.guild;
-var botCount = server.member.cache.filter(member => member.user.bot).size;
-var utentiCount = server.memberCount - botCount;
+client.on("message", (message) => {
 
-client.on("guildMemberAdd", member => {
-    var canale = client.channels.cache.get("905510008010178590")
-    canale.setName("🦽┊Membri: " + utentiCount)
 })
 
 client.on("guildMemberAdd", member => {
-    var canale = client.channels.cache.get("906498797637619752")
-    canale.setName("🤖┊Bots: " + botCount)
 
-})
+    var server = message.member.guild;
+    var botCount = server.member.cache.filter(member => member.user.bot).size;
+    var utentiCount = server.memberCount - botCount;
 
-client.on("guildMemberRemove", member => {
     var canale = client.channels.cache.get("905510008010178590")
     canale.setName("🦽┊Membri: " + utentiCount)
-})
 
-client.on("guildMemberRemove", member => {
     var canale = client.channels.cache.get("906498797637619752")
     canale.setName("🤖┊Bots: " + botCount)
-
 })
 
 
+client.on("guildMemberRemove", member => {
 
+    var server = message.member.guild;
+    var botCount = server.member.cache.filter(member => member.user.bot).size;
+    var utentiCount = server.memberCount - botCount;
+
+    var canale = client.channels.cache.get("905510008010178590")
+    canale.setName("🦽┊Membri: " + utentiCount)
+
+    var canale = client.channels.cache.get("906498797637619752")
+    canale.setName("🤖┊Bots: " + botCount)
+})
 
