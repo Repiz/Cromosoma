35376b52d6abc1dceb521c43cb2d7d6e65db1996
Repiz <93680058+Repiz@users.noreply.github.com>
2021-11-06 15:11:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
-const client = new Discord.Client()
+const client = new Discord.Client(
+{intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MEMBER_ADD", "GUILD_MESSAGE_REACTIONS", "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "DIRECT_MESSAGE_TYPING"]}
+);
 
 
 client.login(process.env.token);
