10b5278bae35019f54fb07c6395673934cd5980a
Repiz <93680058+Repiz@users.noreply.github.com>
2021-11-06 16:11:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(
-{intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MEMBER_ADD", "GUILD_MESSAGE_REACTIONS", "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "DIRECT_MESSAGE_TYPING"]}
+    {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "DIRECT_MESSAGE_TYPING"]}
 );
 
 
@@ -18,28 +18,19 @@ client.on("message", (message) => {
 
 client.on("guildMemberAdd", member => {
 
-    var server = message.member.guild;
-    var botCount = server.member.cache.filter(member => member.user.bot).size;
-    var utentiCount = server.memberCount - botCount;
+    var utentiCount = server.memberCount - 11;
 
     var canale = client.channels.cache.get("905510008010178590")
     canale.setName("🦽┊Membri: " + utentiCount)
-
-    var canale = client.channels.cache.get("906498797637619752")
-    canale.setName("🤖┊Bots: " + botCount)
 })
 
 
 client.on("guildMemberRemove", member => {
 
-    var server = message.member.guild;
-    var botCount = server.member.cache.filter(member => member.user.bot).size;
-    var utentiCount = server.memberCount - botCount;
+    var utentiCount = server.memberCount - 11;
 
     var canale = client.channels.cache.get("905510008010178590")
     canale.setName("🦽┊Membri: " + utentiCount)
 
-    var canale = client.channels.cache.get("906498797637619752")
-    canale.setName("🤖┊Bots: " + botCount)
 })
 
