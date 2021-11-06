dfec9680ed9730f12b9c340f9120f05c697184a6
Repiz <93680058+Repiz@users.noreply.github.com>
2021-11-06 12:05:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -19,11 +19,19 @@ var utentiCount = server.memberCount - botCount;
 client.on("guildMemberAdd", member =>{
     var canale = client.channels.cache.get("905510008010178590")
     canale.setName("🦽┊Membri: " + utentiCount)
+    
+    var canale2 = client.channels.cache.get("906498797637619752")
+    canale2.setName("🤖┊Bots: " + botCount)
+
 });
 
 client.on("guildMemberRemove", member =>{
     var canale = client.channels.cache.get("905510008010178590")
     canale.setName("🦽┊Membri: " + utentiCount)
+
+    var canale2 = client.channels.cache.get("906498797637619752")
+    canale2.setName("🤖┊Bots: " + botCount)
+
 });
 
 
