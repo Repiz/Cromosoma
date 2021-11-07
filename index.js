f7e087516cdca91f7b891d98c07d8584e1d23d05
Repiz <93680058+Repiz@users.noreply.github.com>
2021-11-07 18:20:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const Discord = require("discord.js");
 const client = new Discord.Client(
     {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "DIRECT_MESSAGE_TYPING"]}
 );
+const ytch =require("yt-channel-info");
 
 
 client.login(process.env.token);
 
-client.on("messageCreate", (message) => {
-    if (message.content == "https://tenor.com/view/sussy-baka-sus-dog-yo-momma-gif-21425378"){
-        message.delete();
-    }
-});
+
+setInterval(function(){
+    var canale = client.channels.cache.get("906952878294441994")
+    ytch.getChannelInfo("UCbLFhYI_nmzjK3yBvyPCdnQ")
+    .then(response => {
+        canale.setNmae(`🔴┊Iscritti: ${response.subscriberCount}`)
+    })
+}, 1000 * 60)
+
+
+
+
 
 client.on("message", (message) => {
 
