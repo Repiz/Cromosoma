221ce1ce1ed36e094b942a05a03198045d99bae6
Repiz <93680058+Repiz@users.noreply.github.com>
2021-11-10 18:22:02 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const ytch = require("yt-channel-info");
 
 client.login(process.env.token);
 
+client.on("message", (message) => {
+
+})
 
 setInterval(function(){
     var canale = client.channels.cache.get("906952878294441994")
@@ -17,9 +20,29 @@ setInterval(function(){
 }, 1000 * 900)
 
 
-client.on("message", (message) => {
+var embed = new Discord.MessageEmbed()
+    .setColor("#ffff00")
+    .setTitle("ID Server vanilla di il_boge")
+    .setURL("https://www.twitch.tv/il_boge")
+    .setAuthor("Cromosoma", "https://cdn.discordapp.com/attachments/784431869692870716/904782396577177700/Cromosoma.png")
+    .setDescription("L'ID del server vanilla è " + "**" + "futuro id server" + "**")
+    .setThumbnail("https://images-na.ssl-images-amazon.com/images/I/418cEZfh8-L.jpg")
+    .addField("⚠️⚠️ Avvertenza ⚠️⚠️", " Ti ricordo di non condividere questo ID con nessuno, e ripeto nessuno, per evitare spiacevoli conseguenze nel server vanilla. Grazie", true)
+    .setFooter("Hai utilizzato il comando .idvanilla")
+    .setTimestamp();
 
+
+client.on("message", (message) => {
+    if(message.content == ".idvanilla"){
+        if(message.member.roles.cache.has("884046793728471070")){
+            message.author.send(embed)
+        }
+        else{
+            message.channel.send(message.author.toString() + " non ha il ruolo 🥶Cromosoma Supremo🥶 renny caro!")
+        }
+    }
 })
+
 
 client.on("guildMemberAdd", member => {
 
