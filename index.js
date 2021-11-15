fea81061bc73b5fa12bdc3654edecc26d205ff09
Repiz <93680058+Repiz@users.noreply.github.com>
2021-11-15 15:47:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client(
-    { partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })&&({intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "DIRECT_MESSAGE_TYPING"]}
+    { partials: ['MESSAGE', 'CHANNEL', 'REACTION'] }
+    
 );
 const ytch = require("yt-channel-info");
 const cooldown = new Set();
