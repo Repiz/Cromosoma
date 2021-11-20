2956bbea5e8ad6cf2fb0e3aa827aafa557871a2e
Repiz <93680058+Repiz@users.noreply.github.com>
2021-11-20 21:58:00 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ client.on("message", (message) => {
 
 client.on("message", message => {
     if (message.content == ".react") {
-        client.message.channel.cache.get("894996521265819728").react(`🟫`);
+        client.message.channels.cache.get("894996521265819728").react(`🟫`);
         }
     }
 )
